21b12cce246777d906ff408d364085ac9e6a0e37
Dio.Ye <diov87@outlook.com>
2024-02-24 20:10:17 +0800

    2024-02-24 puhlish

---
+++ assets/index-5-Jif2uL.js
@@ -0,0 +1,5 @@
+const e={title:"Welcome, I’m Dio Ye",tags:null},n=`<p>Hey, I’m Dio Ye.</p>
+<p>A software engineer from China. I’m interested in new technology, photograph and old film.</p>
+<p>Here is my personal site, I will share some of my thoughts and experiences here.</p>
+<p>You can find posts in the <a href="/posts">Posts</a> page.</p>
+`;export{n as html,e as meta};
